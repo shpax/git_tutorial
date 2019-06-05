e6d78adb6cb0a45498c48cefb55233bdc32f58d1
Maxim Piven <shpax1@gmail.com>
2019-06-05 19:54:04 +0300

    added run quiz

--- js/quiz.js
+++ js/quiz.js
@@ -4,3 +4,24 @@ function createQuiz(questions) {
     answers: null,
   }
 }
+
+function runQuiz(quiz) {
+  const { questions } = quiz;
+
+  quiz.answers = questions.map(askQuestion);
+
+  return quiz;
+} 
+
+const questions = [
+  createQuestion('Name', 'What\'s your name'),
+  createQuestion('Company', 'What\'s your company'),
+  createQuestion('Salary', 'Wanna get paid?', ['Yes', 'No'], 'No'),
+]
+
+const quiz = createQuiz(questions);
+
+
+console.log(
+  runQuiz(quiz)
+);
